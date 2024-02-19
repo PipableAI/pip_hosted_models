64bdd1082bce163fe58b3adb81f2daa1cbda1356
Pratham Gupta <pratham@pipable.ai>
2024-02-19 19:26:26 +0530

    changed prompt

--- backend.py
+++ backend.py
@@ -22,12 +22,17 @@ def raven_prompt(prompt: str):
         torch_dtype="auto",
         device_map="auto",
     )
-    result = pipeline(
-        prompt,
-        max_new_tokens=2048,
-        return_full_text=False,
-        do_sample=False,
-        temperature=0.001,
-    )[0]["generated_text"]
+    result = (
+        pipeline(
+            prompt,
+            max_new_tokens=2048,
+            do_sample=False,
+            temperature=0.001,
+            stop=["<bot_end>"],
+            return_full_text=False,
+        )[0]["generated_text"]
+        .replace("Call:", "")
+        .strip()
+    )
     print(result)
     return result
