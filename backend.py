2320d858fe6c3d3544fc059164daf4dadc345492
Pratham Gupta <pratham@pipable.ai>
2024-02-19 18:54:31 +0530

    Added Raven

--- backend.py
+++ backend.py
@@ -1,12 +1,32 @@
-from fastapi import BackgroundTasks, Depends, FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
 
 @app.post("/test")
-async def register_user(name: str = Form(...)):
+async def register_user(prompt: str = Form(...)):
     try:
-        return PlainTextResponse(content=f"Hello {name}", status_code=200)
+        return raven_prompt(prompt)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+def raven_prompt(prompt: str):
+    from transformers import pipeline
+
+    pipeline = pipeline(
+        "text-generation",
+        model="Nexusflow/NexusRaven-V2-13B",
+        torch_dtype="auto",
+        device_map="auto",
+    )
+    result = pipeline(
+        prompt,
+        max_new_tokens=2048,
+        return_full_text=False,
+        do_sample=False,
+        temperature=0.001,
+    )[0]["generated_text"]
+    print(result)
+    return result
