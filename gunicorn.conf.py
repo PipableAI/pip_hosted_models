4efba03bbf18161e1c008ae6ba5a94b45b82ff2e
Pratham Gupta <pratham@pipable.ai>
2024-02-19 18:59:43 +0530

    changed workers to 2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,4 +7,5 @@ log_file = "-"
 bind = "0.0.0.0:3200"
 timeout = 180
 worker_class = "uvicorn.workers.UvicornWorker"
-workers = (multiprocessing.cpu_count() * 2) + 1
+workers = 2
+# workers = (multiprocessing.cpu_count() * 2) + 1
