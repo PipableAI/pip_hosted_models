54566e8431c2b47020307f33226dec0b0bd99ff0
Pratham Gupta <pratham@pipable.ai>
2024-02-19 19:51:55 +0530

    changed model to global

--- backend.py
+++ backend.py
@@ -1,34 +1,34 @@
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import PlainTextResponse
+import torch
 
 app = FastAPI()
 
-_pipline = None
+a_pipline = pipeline(
+    "text-generation",
+    model="Nexusflow/NexusRaven-V2-13B",
+    torch_dtype="auto",
+    device_map="auto",
+)
+
+from transformers import pipeline
 
 
 @app.post("/raven")
 async def register_user(prompt: str = Form(...)):
     try:
         print("Raven processing ....")
-        return raven_prompt(prompt)
+        res = raven_prompt(prompt)
+        torch.cuda.empty_cache()
+        return res
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 def raven_prompt(prompt: str):
-    global _pipeline
-    if _pipeline is None:
-
-        from transformers import pipeline
-
-        _pipeline = pipeline(
-            "text-generation",
-            model="Nexusflow/NexusRaven-V2-13B",
-            torch_dtype="auto",
-            device_map="auto",
-        )
+    global a_pipline
     result = (
-        _pipeline(
+        a_pipeline(
             prompt,
             max_new_tokens=2000,
             do_sample=False,
