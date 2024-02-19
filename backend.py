691f7d7de9f6d9b3bcabadbdf2e7a65873ab7ed1
Pratham Gupta <pratham@pipable.ai>
2024-02-19 19:30:45 +0530

    changed prompt again

--- backend.py
+++ backend.py
@@ -28,6 +28,7 @@ def raven_prompt(prompt: str):
             max_new_tokens=2048,
             do_sample=False,
             temperature=0.001,
+            stop=["<bot_end>"],
             return_full_text=False,
         )[0]["generated_text"]
         .replace("Call:", "")
